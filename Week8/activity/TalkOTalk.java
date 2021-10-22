093ccfe97ebac148b0d8e3e55060d2e5d9691d5d
amohan <amohan@amohanmacpro.local>
2021-10-22 13:18:17 -0400

    added lab-5 grade report file.

--- Week8/activity/TalkOTalk.java
+++ Week8/activity/TalkOTalk.java
@@ -1,11 +1,18 @@
 class Animal{
+	int code = 1;
 	void talk(){
 		System.out.println("Animals talk through the sounds they make!");
 	}
 }
-class Dog extends Animal{
+class Dog extends Animal{ // extends connects a child class to parent class. 
+	//int code = 2; //member variables new copy of that child class. 
+	//int code = super.code; //member variables new copy of that child class. 
+	String code = "2";
+	//super.code = 10; // dog class can be benefited, cat class 
+	//memmber variable override. 
+	// shadow it but we can overwrite it. 
 	@Override
-	void talk(){
+	void talk(){ // member methods
 		System.out.println("Dogs Bark!");
 	}
 	void legs(){
@@ -14,15 +21,23 @@ class Dog extends Animal{
 }
 class TalkOTalk{
 	public static void main(String[] args){
-		Animal o1 = new Animal();
-		o1.talk();
-		
-		Dog o2 = new Dog();
+		//int x = 10; // stack primitive data type 
+		Animal o1 = new Animal(); // instantiating, memory, user given data type
+		o1.talk();  // instantiation reference data type
+		System.out.println(o1.code);
+		Dog o2 = new Dog();  // child object
 		o2.talk();
 		o2.legs();
+		System.out.println(o2.code);
+		
 		
 		Animal o3 = new Dog(); // upcasting
-		o3.talk();
+		o3.talk(); // animal talk method or is it dog talk method
+		//((Dog)o3).legs(); // inside dog class // down casting 
+		// explicitly downcast
+		//((Dog)o1).legs(); // inside dog class // down casting //illegal 
+		System.out.println(o3.code);
+
 		
 		
 	}
