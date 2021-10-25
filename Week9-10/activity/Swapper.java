f0c7ed5869993010a6a0d7f5d91db2f9ef5fbe7a
amohan <amohan@amohanmacpro.local>
2021-10-25 14:18:03 -0400

    added lab5

--- Week9-10/activity/Swapper.java
+++ Week9-10/activity/Swapper.java
@@ -1,23 +1,29 @@
 class Box{
 	int x;
-	int y;
+	int y;  // two member variables
 	Box(int a, int b){
 		this.x = a;
 		this.y = b;
-	}
+	} // constructor, speacial member methods instantiate you create an object 
 }
-public class Swapper{
-	public static void swap(int x, int y){
-		int temp = x;
-		x = y;
-		y = temp;
+public class Main{
+	public static void swap(Box b){ // formal parameter is not equal to actual parameter
+		// formal parameter is equal to actual parameter, same copy pass by reference
+		int temp = b.x;
+		b.x = b.y;
+		b.y = temp;  // temp?
 	}
 	public static void main(String[] args){
 		int x = 10;
 		int y = 20;
-		System.out.println(x + "\t" + y);
-		swap(x, y);
-		System.out.println(x + "\t" + y);
+		Box b1 = new Box(x, y);
+		/*
+		b1.x = x;
+		b1.y = y;
+		*/
+		System.out.println(b1.x + "\t" + b1.y);
+		swap(b1);  // pass by reference
+		System.out.println(b1.x + "\t" + b1.y);
 
 	}
 }
